5ca2fdcc550c7224e38377e33924f04cb93eb0c6
Rene Bažato <rene.bazato@gmail.com>
2022-03-29 09:18:40 +0200

    začetek z bazami

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -10,8 +10,16 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.ReadLine();
+            BazaZaVajeEntities context = new BazaZaVajeEntities();
+
+            //Vsi dobavitelji
+
+            var x1 = from a in context.DOBAVITELJ
+                     select a;
+            foreach(var y in x1)
+            {
+                Console.WriteLine(y.D_IME+" "+y.D_KONTAKT);
+            }
         }
     }
 }
